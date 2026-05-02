6a58a8c1fd67bd180137a24a1f56116e89c33417
Ratnanabh Kulkarni <ratnanabhkulkarni@Ratnanabhs-MacBook-Air.local>
2026-05-02 10:52:27 +0530

    Updated all search routes to use Promises and Async-Await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -2,39 +2,46 @@ const express = require('express');
 let books = require("./booksdb.js");
 const public_users = express.Router();
 
-public_users.get('/', function (req, res) {
-  res.status(200).send(JSON.stringify(books, null, 4));
+// Task 10: Get all books using Async-Await
+public_users.get('/', async function (req, res) {
+  const get_books = () => {
+    return new Promise((resolve) => {
+      resolve(books);
+    });
+  };
+  const allBooks = await get_books();
+  res.status(200).send(JSON.stringify(allBooks, null, 4));
 });
 
+// Task 11: Get book details based on ISBN using Promises
 public_users.get('/isbn/:isbn', function (req, res) {
   const isbn = req.params.isbn;
-  if (books[isbn]) {
-    res.status(200).send(JSON.stringify(books[isbn], null, 4));
-  } else {
-    res.status(404).json({message: "Book not found"});
-  }
+  new Promise((resolve, reject) => {
+    if (books[isbn]) resolve(books[isbn]);
+    else reject("Book not found");
+  })
+  .then(book => res.status(200).send(JSON.stringify(book, null, 4)))
+  .catch(err => res.status(404).json({message: err}));
 });
 
+// Task 12: Get book details based on author using Promises
 public_users.get('/author/:author', function (req, res) {
   const author = req.params.author;
-  let filtered_books = Object.values(books).filter(book => book.author === author);
-  res.status(200).send(JSON.stringify(filtered_books, null, 4));
+  new Promise((resolve) => {
+    let filtered = Object.values(books).filter(b => b.author === author);
+    resolve(filtered);
+  })
+  .then(filtered => res.status(200).send(JSON.stringify(filtered, null, 4)));
 });
 
+// Task 13: Get book details based on title using Promises
 public_users.get('/title/:title', function (req, res) {
   const title = req.params.title;
-  let filtered_books = Object.values(books).filter(book => book.title === title);
-  res.status(200).send(JSON.stringify(filtered_books, null, 4));
-});
-
-// Task 6: Get book reviews
-public_users.get('/review/:isbn', function (req, res) {
-  const isbn = req.params.isbn;
-  if (books[isbn]) {
-    res.status(200).send(JSON.stringify(books[isbn].reviews, null, 4));
-  } else {
-    res.status(404).json({message: "Book not found"});
-  }
+  new Promise((resolve) => {
+    let filtered = Object.values(books).filter(b => b.title === title);
+    resolve(filtered);
+  })
+  .then(filtered => res.status(200).send(JSON.stringify(filtered, null, 4)));
 });
 
 module.exports.general = public_users;
